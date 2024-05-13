53a7164b3b3dbd0fe34ce371637fe1a66248672c
Trọng Tùng <108003841+TrTung216@users.noreply.github.com>
2024-05-13 14:19:40 +0700

    Update web_thidua.sql

--- web_thidua.sql
+++ web_thidua.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 13, 2024 lúc 06:05 AM
+-- Thời gian đã tạo: Th5 13, 2024 lúc 09:19 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.0.28
 
@@ -40,6 +40,35 @@ CREATE TABLE `db_users` (
 
 INSERT INTO `db_users` (`id_lop`, `username`, `pass`, `role`) VALUES
 ('10A1', '10a1thpttienthinh', '12345678', 'users'),
+('10A10', '10a10thpttienthinh', '12345678', 'users'),
+('10A2', '10a2thpttienthinh', '12345678', 'users'),
+('10A3', '10a3thpttienthinh', '12345678', 'users'),
+('10A4', '10a4thpttienthinh', '12345678', 'users'),
+('10A5', '10a5thpttienthinh', '12345678', 'users'),
+('10A6', '10a6thpttienthinh', '12345678', 'users'),
+('10A7', '10a7thpttienthinh', '12345678', 'users'),
+('10A8', '10a8thpttienthinh', '12345678', 'users'),
+('10A9', '10a9thpttienthinh', '12345678', 'users'),
+('11A1', '11a1thpttienthinh', '12345678', 'users'),
+('11A10', '11a10thpttienthinh', '12345678', 'users'),
+('11A2', '11a2thpttienthinh', '12345678', 'users'),
+('11A3', '11a3thpttienthinh', '12345678', 'users'),
+('11A4', '11a4thpttienthinh', '12345678', 'users'),
+('11A5', '11a5thpttienthinh', '12345678', 'users'),
+('11A6', '11a6thpttienthinh', '12345678', 'users'),
+('11A7', '11a7thpttienthinh', '12345678', 'users'),
+('11A8', '11a8thpttienthinh', '12345678', 'users'),
+('11A9', '11a9thpttienthinh', '12345678', 'users'),
+('12A1', '12a1thpttienthinh', '12345678', 'users'),
+('12A10', '12a10thpttienthinh', '12345678', 'users'),
+('12A2', '12a2thpttienthinh', '12345678', 'users'),
+('12A3', '12a3thpttienthinh', '12345678', 'users'),
+('12A4', '12a4thpttienthinh', '12345678', 'users'),
+('12A5', '12a5thpttienthinh', '12345678', 'users'),
+('12A6', '12a6thpttienthinh', '12345678', 'users'),
+('12A7', '12a7thpttienthinh', '12345678', 'users'),
+('12A8', '12a8thpttienthinh', '12345678', 'users'),
+('12A9', '12a9thpttienthinh', '12345678', 'users'),
 ('Admin', 'admin', '12345678', '1');
 
 -- --------------------------------------------------------
